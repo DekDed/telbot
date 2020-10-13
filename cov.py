cce171dddeaa417ad6fe0b70b278448c3ea9c1a8
Danila Gorshkov <gorshkov1999@inbox.ru>
2020-10-13 19:26:03 +0300

    fix

--- cov.py
+++ cov.py
@@ -14,37 +14,31 @@ def getcorona(country):
     final_message = ""
     get_message_bot = country
     if get_message_bot == "сша":
-        final_message = f"Данные по США: " "\n" 
-        + "Заболевшие: " + str(JHU.US.confirmed)) + "\n" 
-        + "Умершие: " + str(JHU.US.deaths)) + "\n"
-        + "Выздоровевшие: " + str(JHU.US.recovered))
+        final_message = "Данные по США: " + "\n Заболевшие: " + str(JHU.US.confirmed) + "\n Умершие: " + str(JHU.US.deaths) + "\n Выздоровевшие: " + str(JHU.US.recovered)
 
     elif get_message_bot == "россия":
-        final_message = f"Данные по России: " "\n" 
-        + "Заболевшие: " + str(JHU.Russia.confirmed)) + "\n" 
-        + "Умершие: " + str(JHU.Russia.deaths)) + "\n"
-        + "Выздоровевшие: " + str(JHU.Russia.recovered))
+        final_message = "Данные по России: " + "\nЗаболевшие: " + str(JHU.Russia.confirmed) + "\nУмершие: " + str(JHU.Russia.deaths) + "\nВыздоровевшие: " + str(JHU.Russia.recovered)
 
-    elif get_message_bot == "япония":
+    """elif get_message_bot == "япония":
         final_message = f"Данные по Японии: " "\n" 
-        + "Заболевшие: " + str(JHU.Japan.confirmed)) + "\n" 
-        + "Умершие: " + str(JHU.Japan.deaths)) + "\n"
-        + "Выздоровевшие: " + str(JHU.Japan.recovered))
+        + "Заболевшие: " + str(JHU.Japan.confirmed) + "\n" 
+        + "Умершие: " + str(JHU.Japan.deaths) + "\n"
+        + "Выздоровевшие: " + str(JHU.Japan.recovered)
         
     elif get_message_bot == "италия":
         final_message = f"Данные по Италии: " "\n" 
-        + "Заболевшие: " + str(JHU.Italy.confirmed)) + "\n" 
-        + "Умершие: " + str(JHU.Italy.deaths)) + "\n"
-        + "Выздоровевшие: " + str(JHU.Italy.recovered))
+        + "Заболевшие: " + str(JHU.Italy.confirmed) + "\n" 
+        + "Умершие: " + str(JHU.Italy.deaths) + "\n"
+        + "Выздоровевшие: " + str(JHU.Italy.recovered)
     else:
         final_message = f"Данные по всему миру: " "\n" 
-        + "Заболевшие: " + str(JHU.Total.confirmed)) + "\n" 
-        + "Умершие: " + str(JHU.Total.deaths)) + "\n"
-        + "Выздоровевшие: " + str(JHU.Total.recovered))
+        + "Заболевшие: " + str(JHU.Total.confirmed) + "\n" 
+        + "Умершие: " + str(JHU.Total.deaths) + "\n"
+        + "Выздоровевшие: " + str(JHU.Total.recovered)
 
 
 
-    """if final_message == "":
+    if final_message == "":
         date = location[0]['last_updated'].split("T")
         time = date[1].split(".")
         #bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAIgT1-A4QeHcYRTjLeh7Q35tkpYaU2IAAIZBgACu-LZS7x-ZyzJUUkPGwQ')
