4cccf770b195a1383facb82d8635859f7a3db027
Danila Gorshkov <gorshkov1999@inbox.ru>
2020-10-15 21:12:31 +0300

    fix

--- bot.py
+++ bot.py
@@ -26,13 +26,16 @@ def lalala(message):
         elif message.text == 'Привет':
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
             markup.row('Как тебя зовут?', 'Закончить Диалог')
-            bot.send_message(message.chat.id, "Добро пожаловать, {0.first_name}!".format(message.from_user, bot.get_me()), parse_mode='html', reply_markup=markup)
+            bot.send_message(message.chat.id, "Привет, {0.first_name}!".format(message.from_user, bot.get_me()), parse_mode='html', reply_markup=markup)
         elif message.text == 'Как тебя зовут?':
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
             markup.row('😊 Как дела?', 'Закончить Диалог')
             bot.send_message(message.chat.id, "Я - <b>{1.first_name}</b>".format(message.from_user, bot.get_me()), parse_mode='html', reply_markup=markup)
         elif message.text == 'Закончить Диалог':
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            item1 = types.KeyboardButton("Коронавирус")
+            item2 = types.KeyboardButton("Привет")
+            markup.add(item1, item2)
             bot.send_message(message.chat.id, 'До свидания, с вами было приятно общаться!', reply_markup=markup)
         elif message.text == '😊 Как дела?':
             markup = types.InlineKeyboardMarkup(row_width=2)
@@ -83,11 +86,10 @@ def callback_inline(call):
             elif call.data == 'bad':
                 bot.send_message(call.message.chat.id, 'Бывает 😢')
  
-            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="😊 Как дела?",
+            #bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="😊 Как дела?",
                 reply_markup=None)
  
-            bot.answer_callback_query(callback_query_id=call.id, show_alert=False,
-                text="ЭТО ТЕСТОВОЕ УВЕДОМЛЕНИЕ!!11")
+            #bot.answer_callback_query(callback_query_id=call.id, show_alert=False, text="ЭТО ТЕСТОВОЕ УВЕДОМЛЕНИЕ!!11")
  
     except Exception as e:
         print(repr(e))
