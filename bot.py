aaa00b97b73f81014e27e7a60098f2d7b4b24704
Danila Gorshkov <gorshkov1999@inbox.ru>
2020-10-15 21:07:40 +0300

    fix

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ markup.add(item1, item2)
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
-    bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAIfZl9_JejD_V_wc6qkAdxMVrpr4cqAAAIuAQACIpHMAztF5TszquAQGwQ')
+    bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAIfZl9_JejD_V_wc6qkAdxMVrpr4cqAAAIuAQACIpHMAztF5TszquAQGwQ', reply_markup=markup)
     #bot.send_message(message.chat.id, "Добро пожаловать, {0.first_name}!\nЯ - <b>{1.first_name}</b>, бот созданный чтобы быть пожилым сычем.".format(message.from_user, bot.get_me()), parse_mode='html', reply_markup=markup)
  
 @bot.message_handler(content_types=['text'])
